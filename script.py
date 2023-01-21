f96cf981505f64838fa2e3d37a8d96b08db5fcca
guy yefet <guy.yefet@gmail.com>
2023-01-22 00:31:27 +0200

    tags check working

--- script.py
+++ script.py
@@ -5,9 +5,9 @@ EC2_SESSION = boto3.Session(region_name="us-east-1")
 EC2_RESOURCE = boto3.resource('ec2', 'us-east-1')
 # EC2_CLIENT = boto3.client('ec2')
 
-tag = EC2_RESOURCE.Tag('resource_id','key','value')
+# tag = EC2_RESOURCE.Tag('resource_id','key','value')
 # instance_email = ''
-# tag = ''
+tag = {}
 protected_instances = []
 pls_check_instances = []
 
@@ -15,39 +15,32 @@ pls_check_instances = []
 def instance_tags_check():
   instances = EC2_RESOURCE.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
   for instance in instances:
-    for tag in instance.tags:
-      if tag['Value'] == 'Protected':
-        protected_instances.append(instance)
-        print("protected instances:", protected_instances)
-      else:
-        instance.stop()
-        print("stopped instances:", instance.id)
+    if {'Key': 'Status', 'Value': 'Protected'} in instance.tags:
+      protected_instances.append(instance)
+      print("protected instances:", instance.id, instance.tags)
+    else:
+      instance.stop()
+      print("stopped instances:", instance.id, instance.tags)
 
-
-          # if instance.tags == None: 
-          #      instance.stop()
-          #      print("stopped instances:", instance.id)
-          # else: 
-          #   for tag in instance.tags: 
-          #     if tag["Key"] == "Protected":
-          #          protected_instances.append(instance)
-          #          print("protected instances:", protected_instances, tag)
-
-# def check_instance_runtime(instance_email):
-#   for instance in protected_instances:
-#     instance_uptime = datetime.now(timezone.utc) - instance.launch_time
-#     instance_uptime = instance_uptime.days
-#     if instance_uptime >= 7:
-#       pls_check_instances.append(instance)
-#       for tag in instance.tags:
-#         if tag['Key'] == 'Email':
-#           instance_email = tag['Value']
-#           print('instance is running more then a week', pls_check_instances,  instance.id,  instance_email)
-#     elif instance_uptime >= 14:
-#       instance.stop()
-#       print('instance is running more then 2 weeks, instance is stopped', pls_check_instances, instance.id, instance_email)
-#     else: 
-#       print('instance is running less then a week', instance.id)
+def check_instance_runtime():
+  for instance in protected_instances:
+    instance_uptime = datetime.now(timezone.utc) - instance.launch_time
+    instance_uptime = instance_uptime.days
+    if instance_uptime >= 7:
+      print('here')
+      pls_check_instances.append(instance)
+      instance_email = tag['Value']
+      for tag in instance.tags:
+        if tag['Key'] == 'Email':
+          instance_email = tag['Value']
+          print('instance is running more then a week', pls_check_instances,  instance.id,  instance_email)
+    elif instance_uptime >= 14:
+      print('here2')
+      instance.stop()
+      print('instance is running more then 2 weeks, instance is stopped', pls_check_instances, instance.id, instance_email)
+    else: 
+      print('here3')
+      print('instance is running less then a week', instance.id)
 
 # def send_email(instance_email):
 
@@ -90,4 +83,4 @@ def instance_tags_check():
 
 
 instance_tags_check()
-# check_instance_runtime(instance_email)
+check_instance_runtime()
