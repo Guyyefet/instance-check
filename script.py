21903e6129e5b6f897dbe7d19e514e464b178eef
guy yefet <guy.yefet@gmail.com>
2023-01-22 16:27:58 +0200

    def  check_instance_runtime fixed

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ EC2_RESOURCE = boto3.resource('ec2', 'us-east-1')
 # EC2_CLIENT = boto3.client('ec2')
 
 # tag = EC2_RESOURCE.Tag('resource_id','key','value')
-# instance_email = ''
+instance_email = {}
 tag = {}
 protected_instances = []
 pls_check_instances = []
@@ -17,27 +17,27 @@ def instance_tags_check():
   for instance in instances:
     if {'Key': 'Status', 'Value': 'Protected'} in instance.tags:
       protected_instances.append(instance)
-      print("protected instances:", instance.id, instance.tags)
+      print("protected instances:", instance.id, instance.tags, type(instance))
     else:
       instance.stop()
-      print("stopped instances:", instance.id, instance.tags)
+      print("stopped instances:", protected_instances, type(instance))
 
-def check_instance_runtime():
+def check_instance_runtime(instance_email, tag):
   for instance in protected_instances:
     instance_uptime = datetime.now(timezone.utc) - instance.launch_time
-    instance_uptime = instance_uptime.days
-    if instance_uptime >= 7:
+    instance_uptime = instance_uptime.seconds
+    for tag in instance.tags:
+      if tag['Key'] == 'Email':
+          instance_email = tag['Value']
+    if instance_uptime in range(30, 300):
+    # if 300 >= instance_uptime >= 30:
       print('here')
       pls_check_instances.append(instance)
-      instance_email = tag['Value']
-      for tag in instance.tags:
-        if tag['Key'] == 'Email':
-          instance_email = tag['Value']
-          print('instance is running more then a week', pls_check_instances,  instance.id,  instance_email)
-    elif instance_uptime >= 14:
+      print('instance is running more then a week', pls_check_instances, instance_uptime, instance_email)
+    elif instance_uptime >= 300:
       print('here2')
       instance.stop()
-      print('instance is running more then 2 weeks, instance is stopped', pls_check_instances, instance.id, instance_email)
+      print('instance is running more then 2 weeks, instance is stopped', pls_check_instances, instance_uptime, instance_email)
     else: 
       print('here3')
       print('instance is running less then a week', instance.id)
@@ -83,4 +83,4 @@ def check_instance_runtime():
 
 
 instance_tags_check()
-check_instance_runtime()
+check_instance_runtime(tag, instance_email)
